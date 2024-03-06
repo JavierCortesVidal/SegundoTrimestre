5779edd56749d782aa98e7e7a8b3a96557798493
DAM1 <DAM1@PC10>
2024-03-06 08:18:59 +0100

    HOLA

---
+++ ProgramacionBloque4/src/Relacion9Ejercicio5/PersonajeException.java
@@ -0,0 +1,11 @@
+package Relacion9Ejercicio5;
+
+@SuppressWarnings("serial")
+
+public class PersonajeException extends Exception {
+
+
+	public PersonajeException(String msg) {
+		super(msg);
+	}
+}
---
+++ ProgramacionBloque4/src/Relacion9Ejercicio5/TRaza.java
@@ -0,0 +1,6 @@
+package Relacion9Ejercicio5;
+/**
+ * Tipo enumerado para la raza
+ *
+ */
+public enum TRaza {HUMANO,ELFO,ENANO,ORCO}
